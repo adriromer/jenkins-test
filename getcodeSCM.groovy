fba1a90dabfa19df89e8ab13dc61e69c9fcfeb15
adriromer <estoesunacuentademail@gmail.com>
2020-08-24 00:53:48 -0300

    atr

---
+++ getcodeSCM.groovy
@@ -0,0 +1,7 @@
+def call(String repo,String branch,String cred){
+
+
+        checkout([ $class: 'GitSCM', branches: [[name: "${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[ $class: 'CleanBeforeCheckout', $class: 'SubmoduleOption', disableSubmodules: false,  recursiveSubmodules: true, reference: '', trackingSubmodules: false ]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${cred}" , url: "${repo}"]] ])
+    
+
+}  
